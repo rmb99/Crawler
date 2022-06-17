44d272459eb208b7e0fb9ec83a0259d74c741f4c
rmb99 <r96103744>
2022-06-17 14:03:32 +0430

    fix config.py

--- model/config.py
+++ model/config.py
@@ -1,6 +1,4 @@
 class BaseConfig:
-    DEBUG = False
-    FLASK_ENV = 'production'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://pg:password@localhost/kilid?sslmode=disable'
 
