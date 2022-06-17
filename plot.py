cbaea24d04036fe400a70d0cf32a007100b58a15
rmb99 <r96103744>
2022-06-17 13:57:44 +0430

    plot candlestick.png

--- plot.py
+++ plot.py
@@ -1,33 +1,50 @@
-from app.dollar_model import Dollar
+from model.dollar_model import Dollar
 from dollar import get_session
-from app.config import BaseConfig
+from model.config import BaseConfig
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-from mpl_finance import candlestick_ohlc
-import matplotlib.dates as mpl_dates
+import plotly.graph_objects as go
 
 session = get_session(BaseConfig.SQLALCHEMY_DATABASE_URI)
+
+
 def plot_daily_close_value():
     dollar_value = session.query(Dollar).all()
     close_value = []
     time =[]
     for item in dollar_value:
         close_value.append(item.last)
-        time.append((item.miladi_date))
+        time.append(item.miladi_date)
 
     plt.plot(time[1:], close_value[1:])
     plt.autoscale(time[1:])
     plt.title('Daily close value')
     plt.savefig('./daily_close_value.png')
 
+
 def candlestick_chart():
-    dollar_value = session.query(Dollar).all()
+    dollar = session.query(Dollar).all()
     time = []
-    for item in dollar_value:
+    opening = []
+    high = []
+    low = []
+    close = []
+    for item in dollar:
+        time.append(item.miladi_date)
+        opening.append(item.opening)
+        high.append(item.max_price)
+        low.append(item.min_price)
+        close.append(item.last)
 
-    fig, ax = plt.subplots()
+    fig = go.Figure(data=[go.Candlestick(x=time,
+                                         open=opening,
+                                         high=high,
+                                         low=low,
+                                         close=close)])
+    # fig.show()
+    fig.update_layout(title='Daily candlestick chart')
+    fig.write_image("candlestick.png")
 
-    candlestick_ohlc(ax, item.values, width=0.6, colorup='green', colordown='red', alpha=0.8
 
 if __name__ == '__main__':
     plot_daily_close_value()
+    candlestick_chart()
